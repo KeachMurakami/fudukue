267c9dc47644a78fb8ea12bcff7075b8f506afd2
MurakamiKeach <m.keach.11@gmail.com>
2017-07-13 20:16:02 +0900

    initial commit

---
+++ R/startup.R
@@ -0,0 +1,13 @@
+suzuri <-
+  function(){
+    library(devtools)
+    library(fudukue)
+    library(tidyverse)
+    library(magrittr)
+    library(lubridate)
+    library(stringr)
+    library(knitr)
+    library(broom)
+
+    session_info()
+  }
