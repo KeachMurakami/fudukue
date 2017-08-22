e699796244e103c2a6344dbb76dc8b32ec961a8a
MurakamiKeach <m.keach.11@gmail.com>
2017-08-22 15:49:08 +0900

    functions in suzuri() and some standard functions were moved to .onAttach

--- R/standard_funs.R
+++ R/standard_funs.R
@@ -1,19 +1,27 @@
-mean2 <- 
-  function(x) mean(x, na.rm = TRUE)
+standard_funs <-
+  function(){
+    mean2 <<-
+      function(x) mean(x, na.rm = TRUE)
 
-sum2 <-
-  function(x) sum(x, na.rm = TRUE)
+    sum2 <<-
+      function(x) sum(x, na.rm = TRUE)
 
-sd2 <- 
-  function(x) sd(x, na.rm = TRUE)
+    sd2 <<-
+      function(x) sd(x, na.rm = TRUE)
 
-se <- 
-  function(x) sd(x, na.rm = TRUE) / sqrt(sum(!is.na(x)))
+    se <<-
+      function(x) sd(x, na.rm = TRUE) / sqrt(sum(!is.na(x)))
 
-length2 <- 
-  function(x) sum(!is.na(x))
+    length2 <<-
+      function(x) sum(!is.na(x))
 
-`%!in%` <-
-  function (x, table){
-    !match(x, table, nomatch = 0L) > 0L
+    `%!in%` <<-
+      function (x, table){
+        !match(x, table, nomatch = 0L) > 0L
+      }
+  }
+
+suzuri <-
+  function(){
+    warnings("suzuri() is no longer required as was already loaded (.onAttached).")
   }
---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onAttach <- function(...) {
+  library(devtools)
+  library(tidyverse)
+  library(magrittr)
+  library(lubridate)
+  library(stringr)
+  library(knitr)
+  library(broom)
+  library(data.table)
+  library(plotly)
+  fudukue::standard_funs()
+
+}
