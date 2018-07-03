7f03c180adbdfc2c75cd23ffdd23121f74bd98fe
KeachMurakami <m.keach.11@gmail.com>
2018-07-03 16:05:25 +1000

    feat: some libraries are now loaded via Rprofile.site

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,5 @@
 .onAttach <- function(...) {
-  library(devtools)
-  library(tidyverse)
-  library(magrittr)
-  library(lubridate)
-  library(stringr)
-  library(knitr)
-  library(broom)
-  library(data.table)
-  library(DT)
-  library(plotly)
-  
   library(kagari)
   library(soroban)
   fudukue::standard_funs()
-
 }
